f8033867c64a1003693dadb5a19f0c921dde5b47
Ravi Kumar Yadav <ravi.yadav@paytm.com>
2026-04-05 15:26:33 +0530

    Chapter2: using setter injection instead of constructor injection

--- src/main/java/com/ravi/demo1/DemoController.java
+++ src/main/java/com/ravi/demo1/DemoController.java
@@ -9,13 +9,23 @@ public class DemoController {
 
     private Coach coach;
 
+//    Constructor Injection
+//    @Autowired
+//    public DemoController(Coach theCoach){
+//        this.coach = theCoach;
+//    }
+
+//    Setter Injection
+//    any name could be given to method eg: setCoach or doSomeStuff
+//    just Annotate with @Autowired
     @Autowired
-    public DemoController(Coach theCoach){
-        this.coach = theCoach;
+    public void doSomeStuff(Coach theCoach){
+        coach = theCoach;
     }
 
     @GetMapping("/")
     public String getDailyWorkout(){
+        System.out.println("In method getDailyWorkout");
         return coach.getDailyWorkout();
     }
 }
